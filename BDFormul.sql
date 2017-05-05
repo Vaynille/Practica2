477264ed4439cfd89a3153a8544e8cfa81d8e0cb
Vaynille <vaynile1@outlook.com>
2017-05-05 02:08:17 -0600

    Add files via upload

---
+++ BDFormul.sql
@@ -0,0 +1,11 @@
+Create database FormularioBilbioteca
+
+use FormularioBilbioteca
+
+Create table Formulario(
+Nombre nvarchar(50),
+Email nvarchar (50),
+Pass nvarchar (50),
+Genero nvarchar (15),
+Fecha_Nac smalldatetime 
+)
